8966bee6ff877de0373ddbdcd797d709371f957c
Ilya <sange0337@gmail.com>
2025-03-16 16:18:08 +0800

    make search input in the header

--- resources/js/components/TagNavItem.tsx
+++ resources/js/components/TagNavItem.tsx
@@ -13,9 +13,9 @@ export default function TagNavItem({ routeName, children, label }: TagNavItemPro
 
     return (
         <li>
-            <Link href={route(routeName)} className={clsx("flex items-center gap-2 border-b border-gray-200 py-3 text-sm md:border-none md:px-3", isCurrent && 'text-primary-blue bg-gray-neutral md:rounded-lg')}>
+            <Link href={route(routeName)} className={clsx("flex items-center gap-2 border-b border-gray-200 py-3 text-sm md:border-none md:px-3 md:rounded-lg", isCurrent && 'text-primary-blue bg-gray-neutral')}>
                 {children}
-                <span className='text-black'>{label}</span>
+                <span className='body-text'>{label}</span>
                 {isCurrent && <ChevronRight className="ml-auto" />}
             </Link>
         </li>
--- resources/js/layouts/UserLayout.tsx
+++ resources/js/layouts/UserLayout.tsx
@@ -7,10 +7,11 @@ import TagIcon from '@/components/svgs/TagIcon';
 import TagNavItem from '@/components/TagNavItem';
 import { useModalContext } from '@/hooks/useModalContext';
 import { useScreenResize } from '@/hooks/useScreenResize';
-import { Transition } from '@headlessui/react';
-import { Head } from '@inertiajs/react';
+import { Input, Transition } from '@headlessui/react';
+import { Head, Link } from '@inertiajs/react';
 import clsx from 'clsx';
 import lightLogo from '../../images/logo-light.webp';
+import SearchIcon from '@/components/svgs/SeachIcon';
 
 type UserLayoutProps = {
     children: React.ReactElement;
@@ -25,11 +26,7 @@ export default function UserLayout({ children, title }: UserLayoutProps) {
                 <UserLayoutSidebar />
 
                 <div className="flex-1">
-                    <header className="bg-gray-neutral px-4 py-3 sm:px-8 sm:py-6">
-                        <div className="w-24">
-                            <img src={lightLogo} alt="Notes logo" />
-                        </div>
-                    </header>
+                    <UserLayoutHeader />
                     <article className="px-4 py-5 sm:px-8 sm:py-6 md:p-0">{children}</article>
                 </div>
 
@@ -39,13 +36,41 @@ export default function UserLayout({ children, title }: UserLayoutProps) {
     );
 }
 
+function UserLayoutHeader() {
+    return (
+        <header className="bg-gray-neutral px-4 py-3 sm:px-8 sm:py-6 md:py-4.5 md:px-8 md:bg-white md:flex md:items-center md:justify-between md:border-b md:border-gray-200 md:gap-4">
+            <div className="w-24 md:hidden">
+                <img src={lightLogo} alt="Notes logo" />
+            </div>
+
+            <div className='font-bold text-2xl hidden md:block'>
+                All Notes
+            </div>
+
+            <div className='ml-auto hidden md:block relative'>
+            <SearchIcon className='absolute bottom-1/2 translate-y-1/2 left-3 body-text' />
+            <Input
+                type="search"
+                name="search"
+                placeholder="Search..."
+                className="pl-12 text-sm border-colors data-[hover]:bg-gray-neutral data-[focus]:shadow-input w-full rounded-lg border px-4 py-3 outline-none data-[focus]:ring-1"
+            />
+            </div>
+
+            <Link href={route('settings')} className='shrink-0 p-2.5 body-text hidden md:block'>
+                <SettingsIcon />
+            </Link>
+        </header>
+    );
+}
+
 function UserLayoutSidebar() {
     const { showSidebar } = useModalContext();
     const isLarge = useScreenResize();
 
     return (
         <Transition show={showSidebar || isLarge}>
-            <aside className="fixed inset-0 top-12 bottom-14 z-10 overflow-y-auto bg-white px-4 py-5 transition duration-300 ease-in data-[closed]:opacity-0 sm:top-19 sm:bottom-19 sm:px-8 sm:py-6 md:py-3 md:px-4 md:static md:min-h-screen md:w-68 md:border-r md:border-gray-200">
+            <aside className="fixed inset-0 top-12 bottom-14 z-10 overflow-y-auto bg-white px-4 py-5 transition duration-300 ease-in data-[closed]:opacity-0 sm:top-19 sm:bottom-19 sm:px-8 sm:py-6 md:static md:min-h-screen md:w-68 md:border-r md:border-gray-200 md:px-4 md:py-3">
                 <div className="hidden md:block">
                     <div className="mb-4 w-24 py-3">
                         <img src={lightLogo} alt="Notes logo" />
@@ -62,7 +87,7 @@ function UserLayoutSidebar() {
                         </ul>
                     </nav>
                 </div>
-                <p className="mb-4 text-2xl font-bold md:mt-2 md:mb-0 md:border-t md:border-gray-200 md:py-2 md:text-sm md:font-normal md:text-gray-500">
+                <p className="mb-4 text-2xl font-bold md:mt-2 md:mb-0 md:border-t md:border-gray-200 md:px-3 md:py-2 md:text-sm md:font-normal md:text-gray-500">
                     Tags
                 </p>
                 <nav>
